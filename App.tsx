7a13d4d9b0e8fb307a72a0fea7587aab33dc71f4
basegiojunior <basegiojunior@gmail.com>
2020-07-13 14:04:23 -0300

    refactor: adaptaçao para ultima versao expo

--- App.tsx
+++ App.tsx
@@ -1,36 +1,54 @@
 /* eslint-disable global-require */
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Text } from "react-native";
 // import * as Font from "expo-font";
 import { useFonts } from "@use-expo/font";
+import * as Font from "expo-font";
 
 import Index from "./src";
-import Loading from "./src/Components/Loading";
+import Loading from "~/Components/Loading";
 
 const App: React.FC = () => {
-  const [fontsLoaded] = useFonts({
-    "Inter-Bold": require("./src/assets/fonts/Inter/Inter-Bold.ttf"),
-    "Inter-Regular": require("./src/assets/fonts/Inter/Inter-Regular.ttf"),
-    "Inter-SemiBold": require("./src/assets/fonts/Inter/Inter-SemiBold.ttf"),
-    Icomoon: require("./src/assets/fonts/icons/fonts/icomoon.ttf"),
-  });
+  // const [fontsLoaded] = Font.useFonts({
+  //   "Inter-SemiBold": require("./src/assets/fonts/Inter/Inter-SemiBold.ttf"),
+  //   "Inter-Bold": require("./src/assets/fonts/Inter/Inter-Bold.ttf"),
+  //   "Inter-Regular": require("./src/assets/fonts/Inter/Inter-Regular.ttf"),
+  //   Icomoon: require("./src/assets/fonts/icons/fonts/icomoon.ttf"),
+  // });
 
-  // const [fontsLoaded, setFontsLoaded] = useState(true);
-  // const customFonts = {
-  //   "Roboto-Bold": require("./src/assets/fonts/Roboto-Bold.ttf"),
-  //   "Roboto-Italic": require("./src/assets/fonts/Roboto-Italic.ttf"),
-  //   "Roboto-BoldItalic": require("./src/assets/fonts/Roboto-BoldItalic.ttf"),
-  //   "Roboto-Regular": require("./src/assets/fonts/Roboto-Regular.ttf"),
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  // const loadAsync: any = async () => {
+  //   await Font.loadAsync({
+  //     "Inter-SemiBold": require("./src/assets/fonts/Inter/Inter-SemiBold.ttf"),
+  //     "Inter-Bold": require("./src/assets/fonts/Inter/Inter-Bold.ttf"),
+  //     "Inter-Regular": require("./src/assets/fonts/Inter/Inter-Regular.ttf"),
+  //     Icomoon: require("./src/assets/fonts/icons/fonts/icomoon.ttf"),
+  //   });
+  //   setLoaded(true);
   // };
 
   // useEffect(() => {
-  //   Font.loadAsync(customFonts)
-  //     .then(() => {
-  //       setFontsLoaded(true);
-  //     })
-  //     .catch(() => {
-  //       setFontsLoaded(false);
-  //     });
-  // }, [customFonts]);
+  //   loadAsync();
+  // }, []);
+
+  // const [fontsLoaded, setFontsLoaded] = useState(true);
+  const customFonts = {
+    "Inter-SemiBold": require("./src/assets/fonts/Inter/Inter-SemiBold.ttf"),
+    "Inter-Bold": require("./src/assets/fonts/Inter/Inter-Bold.ttf"),
+    "Inter-Regular": require("./src/assets/fonts/Inter/Inter-Regular.ttf"),
+    Icomoon: require("./src/assets/fonts/icons/fonts/icomoon.ttf"),
+  };
+
+  useEffect(() => {
+    Font.loadAsync(customFonts)
+      .then(() => {
+        setFontsLoaded(true);
+      })
+      .catch(() => {
+        setFontsLoaded(false);
+      });
+  }, [customFonts]);
 
   return fontsLoaded ? <Index /> : <Loading visible />;
 };
